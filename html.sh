d28bf346c2aa3da9859e89bdcd61743bc882d5e9
Charles Severance <csev@umich.edu>
2013-12-29 05:33:37 +0000

    Update html version

--- html.sh
+++ html.sh
@@ -1,6 +1,7 @@
 # This needs netpbm and hevea from Darwin Ports
 
-rm -rf html
+rm -rf html/*
+rm html-rise*
 rm html-rise.tex
 sed 's"\\slash "/"g' < rise.tex | sed 's"\\- "-"g' | sed "s/\`\`/\"/g" | sed "s/\'\'/\"/g" | sed s/---/--/g > html-rise.tex
 
@@ -8,13 +9,21 @@ sed 's"\\slash "/"g' < rise.tex | sed 's"\\- "-"g' | sed "s/\`\`/\"/g" | sed "s/
 hevea -O -e latexonly htmlonly png.hva html-rise.tex
 hevea -O -e latexonly htmlonly png.hva html-rise.tex
 
+imagen -png html-rise
+
 # Tweak output HTML for glitches
-sed 's/<DT><FONT SIZE=5>/<dt>/g' < html-rise.html | sed 's"</A></FONT><DD>"</a><dd>"g' | sed 's/^3$//' | sed 's/COLOR=purple/face="sans-serif"/g' > rise.html
-rm html-rise*
+sed 's/<DT><FONT SIZE=5>/<dt>/g' < html-rise.html | sed 's"</A></FONT><DD>"</a><dd>"g' | sed 's/^3$//' | sed 's/COLOR=purple/face="sans-serif"/g' | sed '/^<IMG/s/$/<br>/' > rise.html
 
 echo " "
 echo "Running BeautifulSoup on the HTML ..."
 python fixhtml.py
+
+mv html-rise*png html
+mv rise.html html
+
+rm html-rise*
+rm html.zip
+zip -r html.zip html
 
 echo This is non-fatal apparently: Command not found: endsf
 
